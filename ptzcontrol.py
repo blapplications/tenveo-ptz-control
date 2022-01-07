955233ffc58018a06ff349cd1756cbed889e1482
BuildTools <unconfigured@null.spigotmc.org>
2022-01-07 20:56:38 +0100

    Add Move commands

--- ptzcontrol.py
+++ ptzcontrol.py
@@ -3,36 +3,55 @@ import serial
 
 ser = serial.Serial()
 ser.baudrate = 9600
-ser.port = 'COM4'
-ser.open()
+#ser.port = 'COM4'
+#ser.open()
 print(ser.is_open)
 
+def run(hex):
+    #ser.write(bytearray.fromhex(hex))
+    print('1')
+
 def recallPreset1():
-    ser.write(bytearray.fromhex('FF010007000100'))
+    run('FF010007000109')
 
 def recallPreset2():
-    ser.write(bytearray.fromhex('FF010007000200'))
+    run('FF01000700020A')
 
 def recallPreset3():
-    ser.write(bytearray.fromhex('FF010007000300'))
+    run('FF01000700030B')
 
 def recallPreset4():
-    ser.write(bytearray.fromhex('FF010007000400'))
+    run('FF01000700040C')
 
 def recallPreset5():
-    ser.write(bytearray.fromhex('FF010007000500'))
+    run('FF01000700050D')
 
 def recallPreset6():
-    ser.write(bytearray.fromhex('FF010007000600'))
+    run('FF01000700060E')
 
 def recallPreset7():
-    ser.write(bytearray.fromhex('FF010007000700'))
+    run('FF01000700070F')
 
 def recallPreset8():
-    ser.write(bytearray.fromhex('FF010007000800'))
+    run('FF010007000810')
 
 def recallPreset9():
-    ser.write(bytearray.fromhex('FF010007000900'))
+    run('FF010007000911')
+
+def up():
+    run('FF010008101019')
+
+def down():
+    run('FF010010101031')
+    
+def left():
+    run('FF010004101025')
+    
+def right():
+    run('FF010002101023')
+
+def stop():
+    run('FF010000000001')
 
 root = Tk(className=" PTZ Controller")
 
@@ -47,6 +66,13 @@ p6 = Button(root, text="Preset 6", width=40, command=recallPreset6).pack()
 p7 = Button(root, text="Preset 7", width=40, command=recallPreset7).pack()
 p8 = Button(root, text="Preset 8", width=40, command=recallPreset8).pack()
 p9 = Button(root, text="Preset 9", width=40, command=recallPreset9).pack()
+
+up = Button(root, text="Up", width=40, command=up).pack()
+down = Button(root, text="Down", width=40, command=up).pack()
+left = Button(root, text="Left", width=40, command=up).pack()
+right = Button(root, text="Right", width=40, command=up).pack()
+
+stop = Button(root, text="Stop", width=40, command=up).pack()
 
 
 root.mainloop()
